6fea3ed052ef1c2cab978140c4e5ff38e4d39651
Emmaline Mercer <emmaline.mercer@bsu.edu>
2023-09-20 10:52:18 -0400

    Wrote comments as a TO-DO list

--- src/main/java/edu/bsu/cs222/Updates.java
+++ src/main/java/edu/bsu/cs222/Updates.java
@@ -1,4 +1,15 @@
 package edu.bsu.cs222;
 
 public class Updates {
+/*
+    - 13 most recent changes
+    - reverse-chronological order
+    - time of the changes
+    - date of the changes
+    - one space chr
+    - username of person/editor
+    - newline chr
+
+
+ */
 }
