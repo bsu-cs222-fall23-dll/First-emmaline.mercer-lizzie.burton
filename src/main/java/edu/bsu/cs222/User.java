1aeaaba3a54928acaffb0482cd48bad08eccebc8
Emmaline Mercer <84278341+eewmercer@users.noreply.github.com>
2023-09-20 11:39:49 -0400

    Wrote comments as a TO-DO list (2)

--- src/main/java/edu/bsu/cs222/User.java
+++ src/main/java/edu/bsu/cs222/User.java
@@ -1,6 +1,23 @@
 package edu.bsu.cs222;
 
 public class User {
+    /* - this is the main
+   - we are the users looking for
+     the updates (changes)
+ */
+
+    /* TO-DO:
+        - use JSON to parse from wikipedia
+        - connect that with wikipediaarticle & wikipediaAPI
+        - User will pull data from wikiart & wikiAPI
+          to give the "user" the info they want
+        - ErrorHandling will keep the errors from breaking
+          my laptop
+        - Updates will pull data from wikiart & wikiAPI
+          and hold onto that data?? (up for interpretation)
+        - print list of changes; pull data from Updates
+     */
+    
     private String userName;
     private String userID;
 
