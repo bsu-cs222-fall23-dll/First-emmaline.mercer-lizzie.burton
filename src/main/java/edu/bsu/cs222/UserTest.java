29eda5861807559fd642bb0b3b68ed5e6f50ce65
Emmaline Mercer <emmaline.mercer@bsu.edu>
2023-09-21 07:57:54 -0400

    Added code to read the timestamp from Wikipedia

--- src/main/java/edu/bsu/cs222/UserTest.java
+++
@@ -1,8 +0,0 @@
-package edu.bsu.cs222;
-
-public class UserTest {
-    /*
-    this class needs to have tests to run to check the command line output
-     */
-
-}
--- src/main/java/edu/bsu/cs222/WikipediaAPI.java
+++ src/main/java/edu/bsu/cs222/WikipediaAPI.java
@@ -1,5 +1,45 @@
 package edu.bsu.cs222;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
+import java.util.Scanner;
+
 public class WikipediaAPI {
+
+/**
+ * Reads the article name of a Wikipedia page
+ * should WikipediaArticle use that data to print it out?
+ * or should we send it directly to updates?
+ */
+
+    public static void main(String[] args) {
+        WikipediaRevisionReader revisionReader = new WikipediaRevisionReader();
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        try {
+            String timestamp = revisionReader.getLatestRevisionOf(line);
+            System.out.println(timestamp);
+        } catch (IOException ioException) {
+            System.err.println("Network connection problem: " + ioException.getMessage());
+        }
+    }
+
+    private String getLatestRevisionOf(String articleTitle) throws IOException {
+        String urlString = String.format("", articleTitle);
+        String encodedURLString = URLEncoder.encode(urlString, Charset.defaultCharset());
+        try {
+            URL url = new URL(encodedURLString);
+            URLConnection connection = url.openConnection();
+            connection.setRequestProperty("User-Agent", "WikipediaRevisionReader/0.1 ()");
+            InputStream inputStream = connection.getInputStream();
+        } catch (MalformedURLException malformedURLException) {
+            throw new RuntimeException(malformedURLException);
+        }
+    }
 
 }
