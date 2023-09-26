3c3659d8dcd5587d3f7d02be67ccb02f00ef6b59
Lizzie Burton <ewburton@bsu.edu>
2023-09-26 09:00:09 -0400

    I added code to the articleTest class and removed the s from the names of the class.

---
+++ src/test/java/edu/bsu/cs222/ArticleTest.java
@@ -0,0 +1,43 @@
+package edu.bsu.cs222;
+
+import org.junit.jupiter.api.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ArticleTest {
+
+    private static final String SAMPLE_TITLE = "Sample Article";
+    private Article article;
+    private Revision firstRevision;
+    private Revision secondRevision;
+
+    @BeforeEach
+    public void setUp() {
+        firstRevision = new Revision("JohnDoe", "2023-09-25T12:30:45+01:00");
+        secondRevision = new Revision("JaneDoe", "2023-09-24T11:20:35+01:00");
+        List<Revision> revisions = Arrays.asList(firstRevision, secondRevision);
+        article = new Article(SAMPLE_TITLE, revisions);
+    }
+
+    @Test
+    public void testGetTitle() {
+        Assertions.assertEquals(SAMPLE_TITLE, article.getTitle());
+    }
+
+    @Test
+    public void testGetRevisions() {
+        Assertions.assertTrue(article.getRevisions().contains(firstRevision));
+        Assertions.assertTrue(article.getRevisions().contains(secondRevision));
+    }
+
+    @Test
+    public void testGetLatestRevision() {
+        Assertions.assertEquals(firstRevision, article.getLatestRevision());
+    }
+
+    @AfterEach
+    public void tearDown() {
+        article = null;
+    }
+}
